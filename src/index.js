c9b6e4bbc0b244e9c8c4e9e89c25797325f494e0
Michael Whitaker <Michael.Whitaker@xcentium.com>
2022-05-25 07:36:06 -0500

    Start state for 'routing' lecture

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; ///client';
+import React from "react";
+// import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <App />
-// );
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
