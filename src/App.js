70324dc093b5bbae68f08be611b05b2ec3c3dab0
Michael Whitaker <Michael.Whitaker@xcentium.com>
2022-05-25 09:12:03 -0500

    Main Navigation added with routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  //get the path after the domain
+  return (
+    <div>
+      <MainNavigation/>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/favorites" exact={true}>
+          <FavoritesPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
