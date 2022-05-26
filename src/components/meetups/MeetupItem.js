281e81e7439acab2d84d961d6c7479687e0e0310
Michael Whitaker <Michael.Whitaker@xcentium.com>
2022-05-26 11:25:26 -0500

    Add list JSX comps. Use dummy data.

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,28 @@
+import classes from "../../styles/MeetupItem.module.css";
+
+function MeetupItem(props) {
+  const meetup = props.meetup;
+  const image = meetup.image;
+  const title = meetup.title;
+  const address = meetup.address;
+  const description = meetup.description;
+  const id = meetup.id;
+
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+        <p>{description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To favorites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
