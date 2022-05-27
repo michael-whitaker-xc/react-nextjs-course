1b83681034a2a439f128061bd8b0941abdc74117
Michael Whitaker <Michael.Whitaker@xcentium.com>
2022-05-27 11:08:28 -0500

    Add redirect after submissio to db.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,16 +1,23 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     const dbUrl =
       "https://react-meetup-tutorial-940ff-default-rtdb.firebaseio.com";
     fetch(`${dbUrl}/meetups.json`, {
+      //vanilla js
       method: "POST",
       body: JSON.stringify(meetupData),
       headers: {
-        'Content-Type': 'application/json',
-      }
-    }); //vanilla js
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
